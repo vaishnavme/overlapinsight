7e877e6412f9c33b967c77f1af95604c35b47dc7
Vaishnav <chandurkarvaishnav@gmail.com>
2025-12-18 20:07:19 +0530

    improve: added app icon in navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -2,15 +2,28 @@ import Link from "next/link";
 import ThemeToggle from "./theme-toggle";
 import { Text } from "../ui/text";
 import { app_name, app_paths } from "@/lib/constants";
+import Image from "next/image";
 
 const Navbar = () => (
-  <nav className="h-12 px-4 border-b flex items-center justify-between">
-    <Text
-      render={<Link href={app_paths.home} />}
-      className="font-serif text-lg font-medium"
-    >
-      {app_name}
-    </Text>
+  <nav className="h-12 px-2 sm:px-4 border-b flex items-center justify-between">
+    <div className="flex items-center gap-x-1">
+      <Link href={app_paths.home}>
+        <Image
+          src="/overlapinsight.png"
+          alt={`${app_name} logo`}
+          width={24}
+          height={24}
+          quality={100}
+        />
+      </Link>
+
+      <Text
+        render={<Link href={app_paths.home} />}
+        className="font-serif text-lg font-medium"
+      >
+        {app_name}
+      </Text>
+    </div>
     <ThemeToggle />
   </nav>
 );
