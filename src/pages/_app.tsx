37cb4f5df6878cb3951cd9a82d65bf96770ae3cd
Vaishnav <chandurkarvaishnav@gmail.com>
2025-12-18 18:51:40 +0530

    improve: show top-loader on page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { ReactElement, ReactNode } from "react";
 import { NextPage } from "next";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
+import { PagesTopLoader } from "nextjs-toploader/pages";
 import { app_themes } from "@/lib/constants";
 import AppLayout from "@/components/layout/app-layout";
 import fonts from "@/styles/fonts";
@@ -25,6 +26,11 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
       attribute="class"
       defaultTheme={app_themes.system}
     >
+      <PagesTopLoader
+        color="var(--color-amber-400)"
+        height={2}
+        showSpinner={false}
+      />
       <div
         className={`${fonts.geistSans.variable} ${fonts.geistMono.variable} ${fonts.sourceSerif4.variable} font-sans isolate antialiased`}
       >
